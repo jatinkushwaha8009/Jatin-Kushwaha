b85912d5a6300f37a06b1cf7dab346734c19648f
faizahmed09 <125138583+faizahmed09@users.noreply.github.com>
2024-07-29 17:05:08 +0530

    commit

---
+++ assets/php/send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Set email parameters
+    $to = "jatinkushwaha8009@gmail.com";  
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for contacting us!";
+    } else {
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
+}
+?>
